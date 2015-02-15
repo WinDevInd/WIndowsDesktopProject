8b28a7c07191b98df25cafc34a40b18f7fbd818c
WinDevInd <Windevind@outlook.com>
2015-02-15 14:25:12 +0530

    FinalCommit

--- WebCamTest/FingerPrintApplication/SetupHelper.cs
+++ WebCamTest/FingerPrintApplication/SetupHelper.cs
@@ -11,18 +11,19 @@ namespace FingerPrintApplication
     {
         public static void Init()
         {
-            DateTime UpdatedTime = new DateTime(2015, 2, 9, 23, 59, 59);
-            long curDateTime = 1420309800000;
-            if (UpdatedTime < DateTime.Now)
-            {
-                System.Diagnostics.Debug.WriteLine("Invalid system date time - correct system date time");
-                Application.Exit();
-            }
-            else
-            {
-                System.Diagnostics.Debug.WriteLine("Synced with system date time");
-                Application.Run(new Login());
-            }
+            Application.Run(new Login());           
+            //DateTime UpdatedTime = new DateTime(2015, 2, 9, 23, 59, 59);
+            //long curDateTime = 1420309800000;
+            //if (UpdatedTime < DateTime.Now)
+            //{
+            //    System.Diagnostics.Debug.WriteLine("Invalid system date time - correct system date time");
+            //    Application.Exit();
+            //}
+            //else
+            //{
+            //    System.Diagnostics.Debug.WriteLine("Synced with system date time");
+            //    Application.Run(new Login());
+            //}
 
         }
     }
